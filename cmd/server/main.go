73450f06df6e7be73fb1ebef4661bfa5e6bd1186
Kosolpattanadurong Thitiwat <thitiwatipst@icloud.com>
2025-01-10 11:24:40 +0700

    config port number

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,12 +4,32 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"strconv"
+
 	"github.com/CATISNOTSODIUM/threadkeep-backend/internal/router"
+	"github.com/joho/godotenv"
 )
+
+func loadPortNumber() int {
+	err := godotenv.Load()
+	if err != nil {
+		return 8080 // default port number
+	} 
+	_portNumber := os.Getenv("PORT")
+	portNumber, err := strconv.Atoi(_portNumber)
+	
+	if err != nil {
+		return 8080 // default port number
+	} 
+
+	return portNumber
+}
 
 func main() {
 	r := router.Setup()
-	fmt.Print("Listening on port 8080")
+	portNumber := strconv.Itoa(loadPortNumber())
+	fmt.Print("Listening on port " + portNumber)
 	
-	log.Fatalln(http.ListenAndServe(":8080", r))
+	log.Fatalln(http.ListenAndServe(":" + portNumber, r))
 }
